d16b80dbd23925af371ba622c70e01e7cd72344a
Diwakar <diwakarn1599@gmail.com>
2021-06-30 22:05:05 +0530

    [Diwakar] Quotient and reminder problem completed

--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -9,7 +9,7 @@ namespace BasicPrograms
             Console.WriteLine("Basic Programs :");
 
             // creating menu for basic programs
-            Console.WriteLine("MENU\n1.FlipCoin\n2.Leap Year\n3.Power Of Two\n4.Harmonic Number\n5.Primefactor\n6.Exit");
+            Console.WriteLine("MENU\n1.FlipCoin\n2.Leap Year\n3.Power Of Two\n4.Harmonic Number\n5.Primefactor\n6.Quotient and Reminder\n7.Exit");
 
             Console.WriteLine("Enter the option");
 
@@ -31,8 +31,11 @@ namespace BasicPrograms
                 case 5:
                     PrimeFactors.GetPrimeFactors();
                     break;
-
                 case 6:
+                    QuotientAndReminder.GetQuotientAndReminder();
+                    break;
+
+                case 7:
                     Console.WriteLine("Exited");
                     break;
                 default:
---
+++ BasicPrograms/QuotientAndReminder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPrograms
+{
+    class QuotientAndReminder
+    {
+        public static void GetQuotientAndReminder()
+        {
+            Console.WriteLine("Enter the value of divident");
+            int divident = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            // formula for quotient
+            int quotient = divident / divisor;
+
+            //formula for reminder
+            int reminder = divident % divisor;
+
+            Console.WriteLine("Quotient = "+quotient);
+            Console.WriteLine("Reminder = "+reminder);
+
+        } 
+    }
+}
