5eee5ebb7239c390d0851758b666554285290a32
Diwakar <diwakarn1599@gmail.com>
2021-06-30 13:37:05 +0530

    [Diwakar] Flip coin completed with menu

---
+++ BasicPrograms/FlipCoin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPrograms
+{
+    class FlipCoin
+    {
+        public static void FilpCoinPercentage()
+        {
+            int n,head=0,tail=0;
+            double flip, headPercentage, tailPercentage;
+            Console.WriteLine("Enter The number of times to Flip Coin");
+            n = Convert.ToInt32(Console.ReadLine());
+            Random rand = new Random();
+            if(n>0)
+            {
+                for (int i= 0; i < n; i++)
+                {
+                    flip = rand.NextDouble();
+                    if (flip < 0.5)
+                    {
+                        tail++;
+                    }
+                    else
+                    {
+                        head++;
+                    }
+                }
+                Console.WriteLine("Head = "+head);
+                Console.WriteLine("Tail = "+tail);
+                headPercentage = (double)head / n * 100;
+                tailPercentage = (double)tail / n * 100;
+                Console.WriteLine("Head Percentage = " + headPercentage);
+                Console.WriteLine("Tail Percentage = " + tailPercentage);
+
+
+            }
+            else
+            {
+                Console.WriteLine("You have Entered a negative value...Please Enter a positive value");
+                FilpCoinPercentage();
+            }
+
+        }
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -6,7 +6,28 @@ namespace BasicPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Basic Programs");
+            Console.WriteLine("Basic Programs :");
+
+            // creating menu for basic programs
+            Console.WriteLine("MENU\n1.FlipCoin\n2.Exit");
+
+            Console.WriteLine("Enter the option");
+
+            //Switch case for menu
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    FlipCoin.FilpCoinPercentage();
+                    break;
+                case 2:
+                    Console.WriteLine("Exited");
+                    break;
+                default:
+                    Console.WriteLine("Enter the correct option");
+                    break;
+
+            }
+            
         }
     }
 }
