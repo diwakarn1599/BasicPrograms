dbcf2831e9836bf3eca22340795c7e0e6d25e586
Diwakar <diwakarn1599@gmail.com>
2021-06-30 19:26:54 +0530

    [Diwakar] Power of Two is completed

---
+++ BasicPrograms/PowerOfTwo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPrograms
+{
+    class PowerOfTwo
+    {
+        public static void CheckPowerOfTwo()
+        {
+            Console.WriteLine("Enter the value of N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 31)
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    Console.WriteLine("2 power " + i + " is = " + Math.Pow(2, i));
+                }
+            }
+            else
+            {
+                Console.WriteLine("N value should not exceed 30");
+                CheckPowerOfTwo();
+            }
+        }
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -9,7 +9,7 @@ namespace BasicPrograms
             Console.WriteLine("Basic Programs :");
 
             // creating menu for basic programs
-            Console.WriteLine("MENU\n1.FlipCoin\n2.Leap Year\n3.Exit");
+            Console.WriteLine("MENU\n1.FlipCoin\n2.Leap Year\n3.Power Of Two\n4.Exit");
 
             Console.WriteLine("Enter the option");
 
@@ -22,8 +22,11 @@ namespace BasicPrograms
                 case 2:
                     LeapYear.CheckLeapYear();
                     break;
-
                 case 3:
+                    PowerOfTwo.CheckPowerOfTwo();
+                    break;
+
+                case 4:
                     Console.WriteLine("Exited");
                     break;
                 default:
