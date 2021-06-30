2415ffcad00be8526173dbbde48ad827d8ac5b7a
Diwakar <diwakarn1599@gmail.com>
2021-06-30 20:03:58 +0530

    [Diwakar] Prime factors of given number completed '

--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -9,7 +9,7 @@ namespace BasicPrograms
             Console.WriteLine("Basic Programs :");
 
             // creating menu for basic programs
-            Console.WriteLine("MENU\n1.FlipCoin\n2.Leap Year\n3.Power Of Two\n4.Harmonic Number\n5.Exit");
+            Console.WriteLine("MENU\n1.FlipCoin\n2.Leap Year\n3.Power Of Two\n4.Harmonic Number\n5.Primefactor\n6.Exit");
 
             Console.WriteLine("Enter the option");
 
@@ -28,8 +28,11 @@ namespace BasicPrograms
                 case 4:
                     HarmonicNumber.GetHormonicNumber();
                     break;
-
                 case 5:
+                    PrimeFactors.GetPrimeFactors();
+                    break;
+
+                case 6:
                     Console.WriteLine("Exited");
                     break;
                 default:
